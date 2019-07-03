dfd8ab2c3208f6767f472dc9270f793903b11d30
Daniel Fockler <danielf@mammothhr.com>
2019-07-03 08:03:01 -0700

    WIP working on the trig :P

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,59 @@
 extern crate image;
 
 use image::{ImageBuffer};
+use image::math::utils;
+use std::f32;
 
 fn main() {
+    let scene = vec!(
+        Sphere { pos: (256f32, 256f32, 100f32), radius: 30f32, color: (255, 0, 0) }
+    );
+
     let mut img = ImageBuffer::new(512, 512);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        *pixel = image::Rgba(pathtraced(x, y));
+        let norm = ((x as f32).powi(2) + (y as f32).powi(2) + (512.0 as f32).powi(2)).sqrt();
+        *pixel = image::Rgba(pathtraced(Ray{ pos: (256f32, 256f32, 0f32), dir: ((x as f32) / norm, (y as f32) / norm, 512.0 / norm) }, &scene));
     }
 
     img.save("output.png").unwrap();
 }
 
-fn pathtraced(x: u32, y: u32) -> [u8; 4] {
-    [x as u8, y as u8, 0, 255]
+fn dist(x_1: f32, y_1: f32, z_1: f32, x_2: f32, y_2: f32, z_2: f32) -> f32 {
+    ((x_1 - x_2).powi(2) + (y_1 - y_2).powi(2) + (z_1 - z_2).powi(2)).sqrt()
 }
 
-struct Camera {
-    pos: (u32, u32),
-    dir: (u32, u32)
+fn pathtraced(ray: Ray, scene: &Vec<Sphere>) -> [u8; 4] {
+    let sphere = scene.first().unwrap();
+
+    // Distance between the ray origin and the sphere center
+    // for x
+    let u = (sphere.pos.0 - ray.pos.0, sphere.pos.1 - ray.pos.1, sphere.pos.2 - ray.pos.2);
+    let u_abs = dist(sphere.pos.0, sphere.pos.1, sphere.pos.2, ray.pos.0, ray.pos.1, ray.pos.2);
+    let v_dot_u = u.0 * ray.dir.0 + u.1 * ray.dir.1 + u.2 * ray.dir.2;
+    let puv = (ray.dir.0 * v_dot_u, ray.dir.1 * v_dot_u, ray.dir.2 * v_dot_u);
+    let distance = dist(puv.0, puv.1, puv.2, sphere.pos.0, sphere.pos.1, sphere.pos.2);
+
+    println!("{:?}", u);
+
+    if distance < sphere.radius {
+        [sphere.color.0, sphere.color.1, sphere.color.2, 255]
+    } else {
+        [0, 0, 0, 255]
+    }
 }
 
 // Just use spheres for now
 struct Sphere {
-    pos: (u32, u32),
-    radius: u32,
+    pos: (f32, f32, f32),
+    radius: f32,
     color: (u8, u8, u8)
 }
 
+#[derive(Debug)]
 struct Ray {
-    pos: (u32, u32),
-    dir: (u32, u32),
-    mag: u32
+    pos: (f32, f32, f32),
+    dir: (f32, f32, f32)
 }
 
 // Probably more information need for this
