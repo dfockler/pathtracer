7367bc2e95696bc0591e5209c79d0ca4d4492d72
Daniel Fockler <dfockler13@gmail.com>
2019-07-08 21:48:28 -0700

    Get the trig for the intersection function working

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,16 @@ use std::f32;
 
 fn main() {
     let scene = vec!(
-        Sphere { pos: (256f32, 256f32, 100f32), radius: 30f32, color: (255, 0, 0) }
+        Sphere { pos: (0f32, 0f32, 400f32), radius: 200f32, color: (255, 0, 0) }
     );
 
     let mut img = ImageBuffer::new(512, 512);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let norm = ((x as f32).powi(2) + (y as f32).powi(2) + (512.0 as f32).powi(2)).sqrt();
-        *pixel = image::Rgba(pathtraced(Ray{ pos: (256f32, 256f32, 0f32), dir: ((x as f32) / norm, (y as f32) / norm, 512.0 / norm) }, &scene));
+        let dir_x = (x as f32 - 511.0 / 2.0).round();
+        let dir_y = (-(y as f32) + 511.0 / 2.0).round();
+
+        *pixel = image::Rgba(pathtraced(Ray{ pos: (0.0, 0.0, 0.0), dir: (dir_x / 256.0, dir_y / 256.0, 1.0) }, &scene));
     }
 
     img.save("output.png").unwrap();
@@ -29,12 +31,9 @@ fn pathtraced(ray: Ray, scene: &Vec<Sphere>) -> [u8; 4] {
     // Distance between the ray origin and the sphere center
     // for x
     let u = (sphere.pos.0 - ray.pos.0, sphere.pos.1 - ray.pos.1, sphere.pos.2 - ray.pos.2);
-    let u_abs = dist(sphere.pos.0, sphere.pos.1, sphere.pos.2, ray.pos.0, ray.pos.1, ray.pos.2);
     let v_dot_u = u.0 * ray.dir.0 + u.1 * ray.dir.1 + u.2 * ray.dir.2;
     let puv = (ray.dir.0 * v_dot_u, ray.dir.1 * v_dot_u, ray.dir.2 * v_dot_u);
     let distance = dist(puv.0, puv.1, puv.2, sphere.pos.0, sphere.pos.1, sphere.pos.2);
-
-    println!("{:?}", u);
 
     if distance < sphere.radius {
         [sphere.color.0, sphere.color.1, sphere.color.2, 255]
