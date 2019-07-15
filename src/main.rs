6d1c8ca8120eb5ffbe730aedb50ed10c4b475647
Daniel Fockler <danielf@mammothhr.com>
2019-07-15 09:25:07 -0700

    Correct distance function calculation

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,127 @@
 extern crate image;
 
 use image::{ImageBuffer};
-use image::math::utils;
 use std::f32;
+
+const SIZE: f32 = 512.0;
 
 fn main() {
     let scene = vec!(
-        Sphere { pos: (0f32, 0f32, 400f32), radius: 200f32, color: (255, 0, 0) }
+        Sphere { pos: Vector::new(0f32, 0f32, 400f32), radius: 50f32, color: (255, 0, 0) },
+        // Sphere { pos: Vector::new(20f32, 45f32, 200f32), radius: 40f32, color: (0, 255, 0) },
     );
 
-    let mut img = ImageBuffer::new(512, 512);
+    let mut img = ImageBuffer::new(SIZE as u32, SIZE as u32);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let dir_x = (x as f32 - 511.0 / 2.0).round();
         let dir_y = (-(y as f32) + 511.0 / 2.0).round();
 
-        *pixel = image::Rgba(pathtraced(Ray{ pos: (0.0, 0.0, 0.0), dir: (dir_x / 256.0, dir_y / 256.0, 1.0) }, &scene));
+        *pixel = image::Rgba(pathtraced(Ray{ pos: Vector::new(0.0, 0.0, 0.0), dir: Vector::new(dir_x / 256.0, dir_y / 256.0, 1.0) }, &scene));
     }
 
     img.save("output.png").unwrap();
 }
 
-fn dist(x_1: f32, y_1: f32, z_1: f32, x_2: f32, y_2: f32, z_2: f32) -> f32 {
-    ((x_1 - x_2).powi(2) + (y_1 - y_2).powi(2) + (z_1 - z_2).powi(2)).sqrt()
+fn pathtraced(ray: Ray, scene: &Vec<Sphere>) -> [u8; 4] {
+    let mut color = [0, 0, 0, 255];
+
+    for sphere in scene {
+        match intersect(&ray, &sphere) {
+            Some(new_ray) => {
+                color = [sphere.color.0, sphere.color.1, sphere.color.2, 255]
+            },
+            None => (),
+        }
+    }
+
+    color
 }
 
-fn pathtraced(ray: Ray, scene: &Vec<Sphere>) -> [u8; 4] {
-    let sphere = scene.first().unwrap();
+fn intersect(ray: &Ray, sphere: &Sphere) -> Option<Vector> {
+    // Vector between the sphere center and the ray position
+    let u = sphere.pos.sub(&ray.pos);
 
-    // Distance between the ray origin and the sphere center
-    // for x
-    let u = (sphere.pos.0 - ray.pos.0, sphere.pos.1 - ray.pos.1, sphere.pos.2 - ray.pos.2);
-    let v_dot_u = u.0 * ray.dir.0 + u.1 * ray.dir.1 + u.2 * ray.dir.2;
-    let puv = (ray.dir.0 * v_dot_u, ray.dir.1 * v_dot_u, ray.dir.2 * v_dot_u);
-    let distance = dist(puv.0, puv.1, puv.2, sphere.pos.0, sphere.pos.1, sphere.pos.2);
+    // Dot product of the distance vector and the ray direction
+    let v_dot_u = u.dot(&ray.dir);
+
+    // Scale the normalized direction vector by the dot product
+    let puv = ray.dir.scale(v_dot_u);
+
+    // Find the distance between the sphere and the ray
+    let distance = puv.dist(&sphere.pos);
 
     if distance < sphere.radius {
-        [sphere.color.0, sphere.color.1, sphere.color.2, 255]
+        // Distance between the projection and the sphere
+        let c_2 = (distance.powi(2) + sphere.radius.powi(2)).sqrt();
+        let new_ray = puv.sub(&ray.dir.scale(c_2));
+        
+        Some(new_ray)
     } else {
-        [0, 0, 0, 255]
+        None
     }
 }
 
 // Just use spheres for now
 struct Sphere {
-    pos: (f32, f32, f32),
+    pos: Vector,
     radius: f32,
     color: (u8, u8, u8)
 }
 
 #[derive(Debug)]
+struct Vector {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vector {
+    fn new(x: f32, y: f32, z: f32) -> Vector {
+        Vector {
+            x: x,
+            y: y,
+            z: z,
+        }
+    }
+
+    fn dot(&self, other: &Vector) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    fn scale(&self, factor: f32) -> Vector {
+        Vector {
+            x: self.x * factor,
+            y: self.y * factor,
+            z: self.z * factor,
+        }
+    }
+
+    fn dist(&self, other: &Vector) -> f32 {
+        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2) + (self.z - other.z).powi(2)).sqrt()
+    }
+
+    fn sub(&self, other: &Vector) -> Vector {
+        Vector {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+
+    fn add(&self, other: &Vector) -> Vector {
+        Vector {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Ray {
-    pos: (f32, f32, f32),
-    dir: (f32, f32, f32)
+    pos: Vector,
+    dir: Vector,
 }
 
 // Probably more information need for this
